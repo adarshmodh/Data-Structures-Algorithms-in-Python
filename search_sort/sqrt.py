7156955d61e51c73283d644b9c8c2870a39678be
Adarsh Modh <adarsh.modh@gmail.com>
2025-04-01 00:31:53 -0400

    Update sqrt.py

--- search_sort/sqrt.py
+++ search_sort/sqrt.py
@@ -1,32 +1,37 @@
+"""
+Given a non-negative integer x, return the square root of x rounded down to the nearest integer. The returned integer should be non-negative as well.
 
-# Project 
-# Find the sqrt of a number without using swrt, positive integer, 5, - 2.235 , 2. 1.414 
+You must not use any built-in exponent function or operator.
 
-def binary_search(x,y,num,tol):
-    mid = (x+y)/2
-    if (abs(num - mid**2) <= tol) or mid**2==num:
-        return mid
-    elif mid**2 < num:
-        return binary_search(mid, y, num, tol)
-    else:
-        return binary_search(x, mid, num, tol)
-    
+For example, do not use pow(x, 0.5) in c++ or x ** 0.5 in python.
+ 
 
-def sqrt(num, tol):
-    
-    x = 1
-    
-    while(1):
-        if (x*x == num):
-            break
-        elif (x**2 > num):
-            x = binary_search(x-1,x,num, tol)
-            break
-        else:
-            x += 1
-    
-    return x
+Example 1:
 
-input = 5
+Input: x = 4
+Output: 2
+Explanation: The square root of 4 is 2, so we return 2.
+Example 2:
 
-print(sqrt(2, 0.001))
+Input: x = 8
+Output: 2
+Explanation: The square root of 8 is 2.82842..., and since we round it down to the nearest integer, 2 is returned.
+"""
+
+class Solution:
+    def mySqrt(self, x: int) -> int:
+        if x < 2:
+            return x  # sqrt(0) = 0, sqrt(1) = 1
+        
+        low = 1 
+        high = x // 2
+        while low <= high:
+            mid = (low + high) // 2
+            if mid * mid == x:
+                return mid  # Exact square root
+            elif mid * mid < x:
+                low = mid + 1
+            else:
+                high = mid - 1
+        
+        return high  # The integer part of sqrt(x)
