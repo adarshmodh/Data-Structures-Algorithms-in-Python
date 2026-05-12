d6ea3a395e45ba456d1ec1a43ae000f5287bf073
Adarsh Modh <adarsh.modh@gmail.com>
2026-05-12 14:10:53 -0400

    Implement BFS for calculating max area of islands

--- graphs/max_area_islands.py
+++ graphs/max_area_islands.py
@@ -27,3 +27,43 @@ class Solution(object):
                     count = max(dfs(i,j,grid), count)
                     
         return count
+
+
+
+
+
+
+#####################BFS 
+
+
+class Solution:
+    def maxAreaOfIsland(self, grid: List[List[int]]) -> int:
+        directions = [[1, 0], [-1, 0], [0, 1], [0, -1]]
+        ROWS, COLS = len(grid), len(grid[0])
+        area = 0
+
+        def bfs(r, c):
+            q = deque()
+            grid[r][c] = 0
+            q.append((r, c))
+            res = 1
+
+            while q:
+                row, col = q.popleft()
+                for dr, dc in directions:
+                    nr, nc = dr + row, dc + col
+                    if (nr < 0 or nc < 0 or nr >= ROWS or
+                        nc >= COLS or grid[nr][nc] == 0
+                    ):
+                        continue
+                    q.append((nr, nc))
+                    grid[nr][nc] = 0
+                    res += 1
+            return res
+
+        for r in range(ROWS):
+            for c in range(COLS):
+                if grid[r][c] == 1:
+                    area = max(area, bfs(r, c))
+
+        return area
